69da431bd84cb5ed874c132b0ddfb1f7247d5b5a
kristall000 <bis210800@mail.ru>
2021-08-11 15:16:51 +0300

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+var menubtn = document.getElementsByClassName('header__menu');
+
+console.log(menubtn);
+console.log(document.getElementsByClassName('header'));
+
+
+
+// menubtn.addEventListener('click', function (event) {
+//     document.getElementsByClassName('header').style.backgroundColor = 'white';
+// })
