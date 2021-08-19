87de303d7afa886ec66f692398c6f7578088ca98
kristall000 <bis210800@mail.ru>
2021-08-20 01:05:31 +0300

    pre_1.6_courBgFixed_1.1

--- assets/mains/main.js
+++ assets/mains/main.js
@@ -166,72 +166,32 @@ function newsize() {
     }
 }
 
+//Анимированная прокрутка
+
+// var isScrolling = false;
+
+// window.addEventListener("scroll", throttleScroll, false);
+
+// function throttleScroll(e) {
+//     if (isScrolling == false) {
+//         window.requestAnimationFrame(function () {
+//             dealWithScrolling(e);
+//             isScrolling = false;
+//         });
+//     }
+//     isScrolling = true;
+// }
+
+// function dealWithScrolling(e) {
+//     console.log('scrol');
+//     // do epic stuff    
+// }
+
+
+
+
 // Запуск
 newsize();
 window.addEventListener('resize', function () {
     newsize();
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-!function (a) {
-    "use strict";
-
-    a("a.page-scroll").bind("click", function (e) {
-        var l = a(this);
-
-        a("html, body").stop().animate({
-            scrollTop: a(l.attr("href")).offset().top - 50
-        }
-            , 1250, "easeInOutExpo"), e.preventDefault()
-    }
-    ), a("body").scrollspy({
-        target: ".navbar-fixed-top", offset: 51
-    }
-    ), a(".navbar-collapse ul li a").click(function () {
-        a(".navbar-toggle:visible").click()
-    }
-    ), a("#mainNav").affix({
-        offset: {
-            top: 100
-        }
-    }
-    ), window.sr = ScrollReveal(), sr.reveal(".sr-icons", {
-        duration: 600, scale: .3, distance: "0px"
-    }
-        , 200), sr.reveal(".sr-button", {
-            duration: 1e3, delay: 200
-        }
-        ), sr.reveal(".sr-contact", {
-            duration: 600, scale: .3, distance: "0px"
-        }
-            , 300), a(".popup-gallery").magnificPopup({
-                delegate: "a", type: "image", tLoading: "Loading image #%curr%...", mainClass: "mfp-img-mobile", gallery: {
-                    enabled: !0, navigateByImgClick: !0, preload: [0, 1]
-                }
-                , image: {
-                    tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-                }
-            }
-            )
-}
-    (jQuery);
